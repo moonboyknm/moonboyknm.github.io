c562966a583fa85ac6c56e9ee04ea3fca622485c
moonboyknm <kamlesh.muraay@gmail.com>
2025-08-23 21:56:08 +0530

    V5 RED

--- script.js
+++ script.js
@@ -1,72 +1,47 @@
 // Initialize Animate On Scroll (AOS)
 AOS.init({
-    duration: 1000, // values from 0 to 3000, with step 50ms
-    once: true,     // whether animation should happen only once - while scrolling down
+    duration: 1000, 
+    once: true,     
 });
 
 // Initialize Particles.js
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 100, // Number of particles
+      "value": 100, 
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "#00ffcc" // Particle color
+      "value": "#ff4141" // Particle color - RED
     },
     "shape": {
       "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      }
     },
     "opacity": {
       "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
+      "random": false
     },
     "size": {
       "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
+      "random": true
     },
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#00ffcc", // Line color
+      "color": "#ff4141", // Line color - RED
       "opacity": 0.4,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 2, // Speed of particles
+      "speed": 2,
       "direction": "none",
       "random": false,
       "straight": false,
       "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
     }
   },
   "interactivity": {
@@ -74,7 +49,7 @@ particlesJS("particles-js", {
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "grab" // 'grab' or 'repulse'
+        "mode": "grab"
       },
       "onclick": {
         "enable": true,
@@ -89,23 +64,9 @@ particlesJS("particles-js", {
           "opacity": 1
         }
       },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
       "push": {
         "particles_nb": 4
       },
-      "remove": {
-        "particles_nb": 2
-      }
     }
   },
   "retina_detect": true
